9dbfb8dc3e11f995a220394df06e7f1ac0029d0b
Harshit.Bhimsaria <harshit.bhimsaria@coviam.com>
2019-07-03 10:58:41 +0530

    Commit Routing

--- recipies/app.js
+++ recipies/app.js
@@ -1,16 +1,36 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function ($scope) {
+var app = angular.module('myApp', ['ngRoute']);
+app.controller('homeController', function ($scope) {
     $scope.foodList = [
         { name: "Fries", id: 0 },
         { name: "Burger", id: 1 },
         { name: "Pizza", id: 2 },
         { name: "Noodles", id: 3 }
     ];
+});
+
+app.controller('recipeController', function($scope) {
     $scope.reciepieList = [
         ["Ing1", "Ing2", "Ing3", "Ing4", "Ing5"],
         ["Ing6", "Ing2", "Ing7", "Ing4"],
         ["Ing1", "Ing6", "Ing3", "Ing9", "Ing5", "Ing7", "Ing8"],
         ["Ing1", "Ing2", "Ing3"]
     ];
-    $scope.recipieView = false;
 });
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    
+    .when('/', {
+       templateUrl: 'views/homeView.htm',
+       controller: 'homeController'
+    })
+    
+    .when('/recipe/:foodId', {
+       templateUrl: 'views/recipeView.htm',
+       controller: 'recipeController'
+    })
+    
+    .otherwise({
+       redirectTo: '/'
+    });
+ }]);
