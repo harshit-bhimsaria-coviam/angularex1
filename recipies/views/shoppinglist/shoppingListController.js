c132928e49dcc3781af93014134cb7b51a8cd20d
Harshit.Bhimsaria <harshit.bhimsaria@coviam.com>
2019-07-03 18:36:56 +0530

    Edit Option Added

--- recipies/views/shoppinglist/shoppingListController.js
+++ recipies/views/shoppinglist/shoppingListController.js
@@ -6,4 +6,23 @@ app.controller('shoppingListController', function ($scope, ShoppingList) {
   $scope.removeFromShoppingList =  ShoppingList.removeFromShoppingList;
   $scope.shoppingListItemCount = ShoppingList.shoppingListItemCount;
   $scope.shoppingListQuantityCount = ShoppingList.shoppingListQuantityCount;
+
+  $scope.deleteFromShoppingList = function(x) {
+    $scope.ShoppingList.splice($scope.inList(x), 1);
+  };
+
+  $scope.ingredientList = ["Ing1","Ing2","Ing3","Ing4","Ing5","Ing6","Ing7","Ing8","Ing9"]
+
+  $scope.editStatus = false;
+  $scope.changeEditStatus = function() {
+    $scope.editStatus = !$scope.editStatus;
+  };
+
+  $scope.saveChanges = function() {
+    $scope.changeEditStatus();
+    $scope.ShoppingList.forEach((element, index) => {
+      if(element.quantity <= 0)
+        $scope.ShoppingList.splice(index, 1);
+    });
+  };
 });
